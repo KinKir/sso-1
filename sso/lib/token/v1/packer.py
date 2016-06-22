a8265208981abd9894c6fb06560227a455b7cfe7
Parth Desai <desaiparth08@gmail.com>
2016-06-22 14:38:45 +0530

    - Adding packing method

--- sso/lib/token/v1/packer.py
+++ sso/lib/token/v1/packer.py
@@ -1,9 +1,9 @@
 
-IV_LENGTH = 32
+IV_LENGTH = 2
 
-TAG_LENGTH = 16
+TAG_LENGTH = 2
 
-KEYID_LENGTH = 16
+KEYID_LENGTH = 2
 
 
 def pack(iv, ciphertext, tag, aad, keyid):
@@ -51,9 +51,33 @@ def pack(iv, ciphertext, tag, aad, keyid):
 
 
 def unpack(packed):
-    meta = packed[0:2]
 
+    read_length = 0
+
+    meta = packed[0:9]
     aad_length = meta[0]
-    ciphertext_length = meta[1]
+    ciphertext_length = int.from_bytes(meta[1:9], 'big')
+    read_length += len(meta)
+
+    iv = packed[read_length:read_length+IV_LENGTH]
+    read_length += IV_LENGTH
+
+    tag = packed[read_length:read_length+TAG_LENGTH]
+    read_length += TAG_LENGTH
+
+    keyid = packed[read_length:read_length+KEYID_LENGTH]
+    read_length += KEYID_LENGTH
+
+    if aad_length == 0:
+        aad = None
+    else:
+        aad = packed[read_length:read_length+aad_length]
+        read_length += aad_length
+
+    ciphertext = packed[read_length:read_length+ciphertext_length]
+    read_length += ciphertext_length
+
+    return iv, tag, keyid, aad, ciphertext, read_length - len(meta)
+
 
 
