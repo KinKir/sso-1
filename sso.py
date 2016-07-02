02f3ae8b6835fc103b5b559fa0a23d2dca470e63
Parth Desai <desaiparth08@gmail.com>
2016-07-02 19:58:52 +0530

    - Initializing of database.

--- sso.py
+++ sso.py
@@ -1,7 +1,9 @@
 from app_factory import create_app
 from blueprints import available_blueprints
+from db import init_db
 
 app = create_app(available_blueprints)
+init_db(app.config['SQLALCHEMY_DB_URI'], app.config['SQLALCHEMY_DB_ECHO'])
 
 if __name__ == '__main__':
     app.run(host=app.config['SERVER_HOST_NAME'], port=app.config['SERVER_PORT'])
