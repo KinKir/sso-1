d37db757e70c93cdec35253491a1060dffc5d402
Parth Desai <desaiparth08@gmail.com>
2016-07-02 15:22:45 +0530

    - Setting up database structure

--- sso.py
+++ sso.py
@@ -3,10 +3,5 @@ from blueprints import available_blueprints
 
 app = create_app(available_blueprints)
 
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
 if __name__ == '__main__':
     app.run(host=app.config['SERVER_HOST_NAME'], port=app.config['SERVER_PORT'])
