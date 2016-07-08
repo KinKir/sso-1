d3330f17a7dfe0d141235a47cb1e3460230946aa
Parth Desai <desaiparth08@gmail.com>
2016-07-08 20:46:54 +0530

    - Add flags

--- db/models/organization.py
+++ db/models/organization.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, Integer
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
 
@@ -7,6 +7,13 @@ from db.utils import guid
 
 from db.models.user_organization_association import user_organization_association
 
+# This both will be part of the 'default' tenant
+ORGANIZATION_TYPE_PERSONAL = 1
+ORGANIZATION_TYPE_BUSINESS = 2
+
+# This will be part of entirely new tenant.
+ORGANIZATION_TYPE_ENTERPRISE = 4
+
 
 class Organization(Base):
     __tablename__ = 'organizations'
@@ -14,6 +21,8 @@ class Organization(Base):
     id = Column(guid.GUID(), primary_key=True)
     logo = Column(String(2048), nullable=False)
     name = Column(String(512), nullable=False)
+
+    flags = Column(Integer, nullable=False)
 
     tenant_id = Column(guid.GUID(), ForeignKey('tenants.id'), nullable=False)
     tenant = relationship('Tenant', back_populates='organizations')
