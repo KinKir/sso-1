807419726355871058e81044a41276037ddcfaaf
Parth Desai <desaiparth08@gmail.com>
2016-07-12 07:32:09 +0530

    - Fixing function signatures.

--- lib/managers/oauth2/token.py
+++ lib/managers/oauth2/token.py
@@ -4,10 +4,13 @@ from lib.managers.oauth2.user_session import UserSessionManager
 
 
 class TokenManager(Manager):
-    def create_user_token(self, client, user, refresh_token_session_id, create_session=True):
+    def create_user_token(self, client, user, refresh_token_session_id, auth_session_id, create_session=True):
         pass
 
     def create_refresh_token(self, client, user, create_session=True):
+        pass
+
+    def get_refresh_token(self, client, user):
         pass
 
     def revoke_token(self, token):
--- lib/managers/oauth2/user_session.py
+++ lib/managers/oauth2/user_session.py
@@ -3,14 +3,14 @@ from db.models.oauth2.user_session import OAuth2UserSession
 
 
 class UserSessionManager(Manager):
-    def create_user_session(self, user_id, refresh_token_session_id):
+    def create_user_session(self, user_id, refresh_token_session_id, auth_session_id):
         pass
 
-    def get_user_sessions(self, user_id, refresh_token_session_id):
+    def get_user_sessions(self, user_id, refresh_token_session_id, auth_session_id):
         pass
 
-    def get_user_session(self, user_id, refresh_token_session_id, user_session_id):
+    def get_user_session(self, user_id, refresh_token_session_id, auth_session_id, user_session_id):
         pass
 
-    def delete_user_session(self, user_id, refresh_token_session_id, user_session_id):
+    def delete_user_session(self, user_id, refresh_token_session_id, auth_session_id, user_session_id):
         pass
