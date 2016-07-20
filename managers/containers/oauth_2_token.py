ce5ab71c0ee40d0bb8a19a535cc1bd6fe414ba60
Parth Desai <desaiparth08@gmail.com>
2016-07-20 19:34:30 +0530

    - Turning on type annotation for some manager

--- managers/containers/oauth_2_token.py
+++ managers/containers/oauth_2_token.py
@@ -1,11 +1,12 @@
 from managers.base import BaseManager
+from containers.oauth_2_token.common import OAuth2TokenInterface
 
 
 class OAuth2TokenManager(BaseManager):
-    def validate_token(self, token):
+    def validate_token(self, token: OAuth2TokenInterface) -> bool:
         pass
 
-    def revoke_token(self, token):
+    def revoke_token(self, token: OAuth2TokenInterface) -> object:
         pass
 
 
--- managers/containers/sso_session_cookie.py
+++ managers/containers/sso_session_cookie.py
@@ -1,10 +1,23 @@
 from managers.base import BaseManager
+from containers.sso_session_cookie.common import SSOSessionCookieInterface
 
 
 class SSOSessionCookie(BaseManager):
-    def validate_cookie(self, cookie, current_stage):
+    def validate_cookie(self, cookie: SSOSessionCookieInterface, current_stage: int) -> bool:
         pass
 
-    def revoke_cookie(self, cookie):
+    def _validate_for_login_started_stage(self, cookie: SSOSessionCookieInterface) -> bool:
+        pass
+
+    def _validate_for_provider_chose_stage(self, cookie: SSOSessionCookieInterface) -> bool:
+        pass
+
+    def _validate_for_provider_execution_stage(self, cookie: SSOSessionCookieInterface) -> bool:
+        pass
+
+    def _validate_for_logged_in_stage(self, cookie: SSOSessionCookieInterface) -> bool:
+        pass
+
+    def revoke_cookie(self, cookie: SSOSessionCookieInterface) -> object:
         pass
 
