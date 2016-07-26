907c9a16b1cc682fc8f281f3bb15f691a292fe18
Parth Desai <desaiparth08@gmail.com>
2016-07-27 00:14:08 +0530

    - Assuming hybrid storage will only deal strings

--- storages/hybrid.py
+++ storages/hybrid.py
@@ -8,7 +8,11 @@ class HybridStorage(object):
 
     _DEFAULT_TIME_DELTA = 10*24*60*60
 
-    _UUID_LENGTH = 16
+    _UUID_HEX_LENGTH = 32
+
+    _PARTIAL_COOKIE = 'r'
+
+    _WHOLE_COOKIE = 'c'
 
     def __init__(self, strict_redis_client, cookie_name, max_cookie_limit=4000, time_delta=_DEFAULT_TIME_DELTA):
         self._redis_client = strict_redis_client
@@ -17,26 +21,27 @@ class HybridStorage(object):
         self._time_delta = time_delta
 
     def store(self, response, storage_id, data, purge_old_data=True):
-        metadata = bytes([0])
+        metadata = self._WHOLE_COOKIE
 
         if len(data) <= (self._max_cookie_limit - 1):
-            response.set_cookie(self._cookie_name, metadata + data, expires=time.time() + self._time_delta)
+            response.set_cookie(self._cookie_name, metadata + data,
+                                expires=time.time() + self._time_delta)
 
             if purge_old_data:
-                self._redis_client.delete(storage_id.hex())
+                self._redis_client.delete(storage_id)
 
             return metadata + data
 
-        metadata = bytes([1])
+        metadata = self._PARTIAL_COOKIE
         if storage_id is None:
             storage_id = uuid.uuid4()
 
-        whole_data = metadata + storage_id.bytes() + data
+        whole_data = metadata + storage_id.hex + data
         cookie_part = whole_data[0:self._max_cookie_limit]
         redis_part = whole_data[self._max_cookie_limit:len(whole_data)]
 
         response.set_cookie(self._cookie_name, cookie_part, expires=time.time() + self._time_delta)
-        self._redis_client.set(storage_id.hex(), redis_part, self._time_delta)
+        self._redis_client.set(storage_id.hex, redis_part, self._time_delta)
         return whole_data
 
     def retrieve(self, request):
@@ -45,18 +50,21 @@ class HybridStorage(object):
             return None
 
         metadata = cookie_part[0]
-        if metadata != 1 and metadata != 0:
+        if metadata != self._PARTIAL_COOKIE and metadata != self._WHOLE_COOKIE:
             return None
 
-        if len(cookie_part) < len(metadata) + self._UUID_LENGTH:
+        if metadata == self._WHOLE_COOKIE:
+            return cookie_part[1:]
+
+        if len(cookie_part) < (len(metadata) + self._UUID_HEX_LENGTH):
             return None
 
-        storage_id = uuid.UUID(bytes=cookie_part[len(metadata):self._UUID_LENGTH+len(metadata)])
+        storage_id = uuid.UUID(hex=cookie_part[len(metadata):self._UUID_HEX_LENGTH+len(metadata)])
 
-        redis_part = self._redis_client.get(storage_id.hex())
+        redis_part = self._redis_client.get(storage_id)
         if redis_part is None:
             return None
 
-        self._redis_client.expire(redis_part, self._time_delta)
+        self._redis_client.expire(storage_id, self._time_delta)
 
-        return storage_id, cookie_part[(len(metadata)+self._UUID_LENGTH):len(cookie_part)] + redis_part
+        return storage_id, cookie_part[(len(metadata)+self._UUID_HEX_LENGTH):len(cookie_part)] + redis_part
