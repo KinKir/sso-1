d26b439a3c0afedd08dc1e578829b7e149b9c1ee
Parth Desai <desaiparth08@gmail.com>
2016-07-28 20:21:32 +0530

    - Initial impl of stack, workflow session

--- containers/generic_stack_session/session.py
+++ containers/generic_stack_session/session.py
@@ -3,10 +3,6 @@ from containers.generic_session.session import GenericSession
 
 class GenericStackSession(GenericSession):
 
-    ARGUMENT_KEY = 'a'
-
-    RET_VAL_KEY = 'r'
-
     STORAGE_KEY = 'd'
 
     ARCHIVE_KEY = 'e'
@@ -20,9 +16,9 @@ class GenericStackSession(GenericSession):
         self[self.ID_KEY] = stack_id
         self._next_sid = 0
 
-    def push_session(self, args):
+    def push_session(self):
         current_sid = self._next_sid
-        self[self.STORAGE_KEY][current_sid] = {self.ARGUMENT_KEY: args}
+        self[self.STORAGE_KEY][current_sid] = {}
         self._next_sid += 1
         return self._next_sid
 
@@ -31,12 +27,11 @@ class GenericStackSession(GenericSession):
             raise OverflowError
 
         current_sid = self._next_sid - 1
-        ret_val = self[self.STORAGE_KEY][current_sid].get(self.RET_VAL_KEY)
         self[self.ARCHIVE_KEY][current_sid] = self[self.STORAGE_KEY][current_sid]
 
         del self[self.STORAGE_KEY][current_sid]
         self._next_sid -= 1
-        return ret_val
+        return self._next_sid
 
     def get_current_session(self):
         if self._next_sid == 0:
@@ -44,16 +39,8 @@ class GenericStackSession(GenericSession):
         current_sid = self._next_sid - 1
         return self[self.STORAGE_KEY][current_sid]
 
-    def get_current_session_args(self):
-        session = self.get_current_session()
-        return session.get(self.ARGUMENT_KEY)
-
     def get_session(self, sid):
         return self[self.STORAGE_KEY].get(sid)
-
-    def get_session_args(self, sid):
-        session = self.get_session(sid)
-        return session.get(self.ARGUMENT_KEY)
 
     def get_max_sid_issued(self):
         return self._next_sid - 1
---
+++ containers/sso_workflow_stack_session/session.py
@@ -0,0 +1,45 @@
+from containers.generic_stack_session.session import GenericStackSession
+
+
+class SSOWorkflowStackSession(object):
+
+    ARGS_KEY = 'a'
+
+    SESSIONS = {
+        'SSO_SESSION': {
+            'RELATIVE_SID': 1,
+            'ARGS': ['pcr'],
+            'VALID_STORAGE_KEYS': ['pcr'] + [ARGS_KEY]
+        },
+        'PROVIDER_SESSION': {
+            'RELATIVE_SID': 2,
+            'ARGS': ['pid'],
+            'VALID_STORAGE_KEYS': ['pid'] + [ARGS_KEY]
+        },
+        'PROVIDER_INSTANCE_SESSION': {
+            'RELATIVE_SID': 3,
+            'ARGS': None,
+            'VALID_STORAGE_KEYS': None
+        }
+    }
+
+    ID = 'sso'
+
+    def __init__(self, starting_sid):
+        self._stack_session = GenericStackSession(self.ID)
+        self.__starting_sid = starting_sid
+
+    def is_in_session(self, session_name):
+        pass
+
+    def store_in_current_session(self, key, val):
+        pass
+
+    def get_current_session_args(self):
+        pass
+
+    def transition_to_session(self, session_name, args):
+        pass
+
+    def get_raw_data(self):
+        pass
